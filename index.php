6cfcac171793cc27bc7a45381f467238e337a8d9
Daniea von Arb <daniela.vonarb@openpolytechnic.ac.nz>
2022-12-14 10:11:43 +1300

    create change_status

--- index.php
+++ index.php
@@ -26,7 +26,10 @@ if (file_exists('todo.json')) {
 
 <?php foreach ($todos as $todoName => $todo): ?>
     <div style="margin-bottom: 20px;">
-        <input type="checkbox" <?php echo $todo['completed'] ? 'checked' : '' ?>>
+        <form style="display: inline-block" action="change_status.php" method="post">
+            <input type="hidden" name="todo_name" value="<?php echo $todoName ?>">
+            <input type="checkbox" <?php echo $todo['completed'] ? 'checked' : '' ?>>
+        </form>      
         <?php echo $todoName ?>
         <form style="display: inline-block" action="delete.php" method="post">
             <input type="hidden" name="todo_name" value="<?php echo $todoName ?>">
@@ -35,6 +38,15 @@ if (file_exists('todo.json')) {
     </div>
 
 <?php endforeach; ?>
+
+<script>
+    const checkboxes = document.querySelectorAll('input[type=checkbox]');
+    checkboxes.forEach(ch => {
+        ch.onclick = function () {
+            this.parentNode.submit();
+        };
+    })
+</script>
     
 </body>
 </html>

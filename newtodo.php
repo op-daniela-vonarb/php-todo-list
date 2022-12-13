7425253c760396363cacc949eb2b2c606432aa77
Daniea von Arb <daniela.vonarb@openpolytechnic.ac.nz>
2022-12-14 09:27:01 +1300

    create newtodo

--- newtodo.php
+++ newtodo.php
@@ -5,6 +5,7 @@ $todoName = trim($todoName);
 
 if ($todoName) {
     $json = file_get_contents('todo.json');
-    echo $json;
-    // file_put_contents('todo.json')
+    $jsonArray = json_decode($json, true);
+    $jsonArray[$todoName] = ['completed' => false];
+    file_put_contents('todo.json', json_encode($jsonArray, JSON_PRETTY_PRINT));
 }
